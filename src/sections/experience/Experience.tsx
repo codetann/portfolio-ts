90f84f35302bc13a6bdf344af3d4631bd84987ec
codetann <thetannerthomas@gmail.com>
2022-02-11 22:17:26 -0700

    add text to experience section

--- src/sections/experience/Experience.tsx
+++ src/sections/experience/Experience.tsx
@@ -1,8 +1,22 @@
-import { Section } from "@components";
+import { Section, Spacer } from "@components";
 import { FaBriefcase } from "react-icons/fa";
 import SectionHeader from "src/components/layout/SectionHeader";
+import { Text, TextProps } from "@chakra-ui/react";
 
 type Props = {};
+
+type Styles = {
+  text: TextProps;
+};
+
+// styles
+const styles: Styles = {
+  text: {
+    color: "text.light",
+    textAlign: "center",
+    transition: "all 0.1s ease-in-out",
+  },
+};
 
 export function Experience({}: Props) {
   return (
@@ -13,6 +27,13 @@ export function Experience({}: Props) {
         label="Work Experience"
         heading="Full Stack Developer"
       />
+      <Spacer amount={1} />
+      <Text {...styles.text}>
+        I'm a developer and photographer. I've been working as a developer for
+        over a year and a half, and I've been a photographer for about a year.
+        I've been working on a few projects, but I'm always looking for new
+        challenges.
+      </Text>
     </Section>
   );
 }
